6e74dbf87dd39e9ba1099aef4c542b99a3cc7148
Seu Pedro <33115289+seupedro@users.noreply.github.com>
2025-03-19 23:07:09 -0300

    Initial commit

---
+++ src/main/java/email/openrouter/OpenrouterEmailApplication.java
@@ -0,0 +1,13 @@
+package email.openrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrouterEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenrouterEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/email/openrouter/OpenrouterEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package email.openrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrouterEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
